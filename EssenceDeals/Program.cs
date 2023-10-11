d1c88331adebd7e1c13367564568e3217ed9628c
Nicolas Secilio Rodrigues <79922185+NicolasR-S@users.noreply.github.com>
2023-10-11 14:05:27 -0300

    Update Program.cs

--- EssenceDeals/Program.cs
+++ EssenceDeals/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<Contexto>
-    (options => options.UseSqlServer("Server=tcp:cp2-sqlserver.database.windows.net,1433;Initial Catalog=crud;Persist Security Info=False;User ID=nicolas;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"));
+    (options => options.UseSqlServer("Server=tcp:cp2-sqlserver.database.windows.net,1433;Initial Catalog=Contexto;Persist Security Info=False;User ID=nicolas;Password=Banco@fiap2tds;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"));
 
 
 var app = builder.Build();
