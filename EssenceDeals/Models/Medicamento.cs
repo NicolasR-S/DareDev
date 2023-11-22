18f6a538c3b2cc6ccc0e82ad3b80e50da7cce041
Nicolas Secilio Rodrigues <79922185+NicolasR-S@users.noreply.github.com>
2023-11-22 11:24:13 -0300

    Update Medicamento.cs

--- EssenceDeals/Models/Medicamento.cs
+++ EssenceDeals/Models/Medicamento.cs
@@ -7,23 +7,23 @@ namespace EssenceDeals.Models
     [Table("Medicamento")]
     public class Medicamento
     {
-        [Column("CodCliente")]
-        [Display(Name="CodigoCliente")]
+        [Column("CodMedicamento")]
+        [Display(Name="CodigoMedicamento")]
         [Key]
         public int CodMedicamento { get; set; }
 
 
-        [Column("Nome")]
-        [Display(Name="Nome")]
+        [Column("NomeMedicamento")]
+        [Display(Name="NomeMedicamento")]
         public string NomeMedicamento { get; set; }
 
 
-        [Column("Telefone")]
-        [Display(Name ="Telefone")]
+        [Column("destino")]
+        [Display(Name ="destino")]
         public string destino { get; set; }
 
-        [Column("Email")]
-        [Display(Name = "E-mail")]
+        [Column("TipoMedicamento")]
+        [Display(Name = "TipoMedicamento")]
         public string TipoMedicamento { get; set; }
 
 
