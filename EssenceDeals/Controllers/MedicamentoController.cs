38c42baa3b7551abde69e7fa3f41c0ca7301b139
Nicolas Secilio Rodrigues <79922185+NicolasR-S@users.noreply.github.com>
2023-11-24 09:31:35 -0300

    Update MedicamentoController.cs

--- EssenceDeals/Controllers/MedicamentoController.cs
+++ EssenceDeals/Controllers/MedicamentoController.cs
@@ -161,12 +161,7 @@ namespace EssenceDeals.Controllers
           return (_context.Medicamento?.Any(e => e.CodMedicamento == id)).GetValueOrDefault();
         }
 
-        app.UseEndpoints(endpoints =>
-        {
-          endpoints.MapControllerRoute(
-              name: "default",
-              pattern: "{controller=Medicamento}/{action=Index}/{id?}");
-         });
+        
     }
 
     
